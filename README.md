d76d944edb868bd7e500ba4c6a8e6bfc3b1461b5
Max Shishkin <angryobject@users.noreply.github.com>
2017-08-12 20:14:05 +0300

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+# nodelist-foreach
+
+A polyfill to add `forEach` on `NodeList` for non supporting browsers (e.g. IE). 
+
+```javascript
+import 'nodelist-foreach';
+
+querySelectorAll('.my-component').forEach(doStuff);
+```
+
+## Installation
+
+npm:
+
+```bash
+npm i -S nodelist-foreach
+```
+
+yarn:
+
+```bash
+yarn add nodelist-foreach
+```
---
+++ nodelist-foreach.js
@@ -0,0 +1,14 @@
+(function(self) {
+  'use strict';
+
+  if (self.NodeList.prototype.forEach) { 
+    return;
+  }
+
+  self.NodeList.prototype.forEach = function forEach(cb, thisArg) {
+    for (let i = 0; i < this.length; i++) {
+      thisArg ? cb.call(thisArg, this[i], i, this) : cb(this[i], i, this);
+    }
+  };
+})(typeof self !== 'undefined' ? self : this);
+
